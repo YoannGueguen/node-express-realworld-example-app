0c936c5e13d45c14f77fe305d1d3ce7bea884bc1
victor duchenet <victor.duchenet@ynov.com>
2019-05-17 13:01:05 +0200

    test model

--- unit-tests/article.model.test.js
+++ unit-tests/article.model.test.js
@@ -1,24 +1,26 @@
-const assert = require('assert');
-var mongoose = require('mongoose');
-require('../models/User');
-require('../models/Comment');
-require('../models/Article');
-const Article = mongoose.model('Article');
-const mockResponse = require('./mocks/responses.mock.js');
+const User = require("../models/User");
+const Article = require("../models/article");
 
-const articleCtrl = require('../controllers/articles.controller');
 
-mongoose.connect('mongodb://localhost/conduit-test');
-
-describe('Article module should', function() {
+describe('Article model should', function() {
 
     it('slug is lowercase', function () {
-        const mySlug = 'ItIsASlug';
-        const testResult = 'itisaslug';
+        const testSlug = "fjeiofrTtregorejvEFZfFEJFO";
+        const testResultSlug = testSlug.toLocaleLowerCase();
+        const newArticle = new Article();
+        newArticle.title= testSlug;
+        newArticle.describe = "zerzrzer";
+        // newArticle.author = new User();
+        newArticle.body = "bodddyd_è)àç_è-(-è_ç_è-t(-yèu_çào_èu_çàç_èu_çà_i";
+        newArticle.validate();
+        expect(newArticle.slug).toContain(testResultSlug);
+    });
 
-        const newArticle = new Article({
-
-        });
+    it('by default favoriteCounts is equal to 0', function () {
+        const newArticle = new Article();
+        newArticle.title = "";
+        newArticle.validate();
+        expect(newArticle.favoritesCount).toBe(0);
     });
 
 });
