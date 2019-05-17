982859f8927d11366e1f2395bf976c0ebf87a941
KevSib <ksibue@gmail.com>
2019-05-17 09:15:49 +0200

    correct postman automatic test to replay them lot of time

--- unit-tests/users.route.test.js
+++ unit-tests/users.route.test.js
@@ -1,3 +1,31 @@
 describe('Users routes', function() {
 
+    it('get error if there no email in body', function () {
+
+    });
+
+    it('get error if there no password in body', function () {
+
+    });
+
+    it('can user\'s username update', function () {
+
+    });
+
+    it('can user\'s email update', function () {
+
+    });
+
+    it('can user\'s bio update', function () {
+
+    });
+
+    it('can user\'s password update', function () {
+
+    });
+
+    it('request a unknown user must send error 401', function () {
+
+    });
+
 });
